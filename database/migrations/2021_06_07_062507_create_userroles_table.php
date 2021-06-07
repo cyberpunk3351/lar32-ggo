6305aaf05ec017eaa02ca4049d1b2f8334c4be17
cyberpunk3351 <drummer.ilya@gmail.com>
2021-06-07 14:50:41 +0700

    Initial commit

---
+++ database/migrations/2021_06_07_062507_create_userroles_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserrolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('userroles', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('userroles');
+    }
+}
